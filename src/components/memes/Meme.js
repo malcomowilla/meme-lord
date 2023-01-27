a5d007600c82608da8c8b09345c544eee232b1d8
Samueelx <gsammygitau@gmail.com>
2023-01-27 07:34:02 +0300

    randomize the memes on click.

--- src/components/memes/Meme.js
+++ src/components/memes/Meme.js
@@ -40,7 +40,7 @@ function Memes() {
                     type="button"
                     data-mdb-ripple="true"
                     data-mdb-ripple-color="light"
-                    onClick={() => setMemeIndex(memeIndex + 1)}
+                    onClick={() => setMemeIndex(Math.floor(Math.random() * 100) + 1)}
                     class="inline-block px-6 py-2.5 bg-orange-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-900 hover:shadow-lg focus:bg-stone-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-stone-900 active:shadow-lg transition duration-150 ease-in-out">
                     skip{" "}
                 </button>
